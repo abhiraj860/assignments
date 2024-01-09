b5c757b86e5637c054c4bf32e1289c255a0cd690
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-10 00:29:58 +0530

    basic-change-function-added-week-5

--- week-5/level-1/project-cards/frontend/src/App.jsx
+++ week-5/level-1/project-cards/frontend/src/App.jsx
@@ -1,5 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
+// const prop = {
+  //   name: 'Lokeshwar',
+  //   description: 'A TA in the 100xDevs Cohort 2.0',
+  //   interests: ['Ionic', 'Open Source', 'App Dev'],
+  //   linkedin: "https://www.linkedin.com",
+  //   twitter: "https://twitter.com"
+  // };
+
+function App() {
+  const [props, setProps] = useState({name: 'Your name',
+                                description: 'Your description',
+                                interests: ['Your interests'],
+                                linkedin: "https://www.linkedin.com",
+                                twitter: "https://twitter.com"
+                              });
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+                            
+  const [counter, setCounter] = useState(1);
+  function inputNameHandler(event) {
+    setName(event.target.value);
+  }
+  
+  function inputDescriptionHandler(event) {
+    setDescription(event.target.value);
+  }
+
+  function clickHandler() {
+    let copy = {...props};
+    copy.name = name;
+    copy.description = description;
+    setProps(copy);
+  }
+
+
+  return <div className='page'>
+    <Input clickHandler={clickHandler} inputNameHandler={inputNameHandler} inputDescriptionHandler={inputDescriptionHandler}/>
+    <Card props={props}/>
+  </div>
+}
+
+function Input({clickHandler, inputNameHandler, inputDescriptionHandler}) {
+  return <div>
+    <input onBlur={inputNameHandler} className='inputname' type='text' placeholder='Enter your name'/>
+    <br></br>
+    <input onBlur={inputDescriptionHandler} className='inputdescription' type='text' placeholder='Give a short description'/>
+    <br></br>
+    <button onClick={clickHandler}>Create Card</button>
+  </div>
+}
 
 function Card({props}) {
   return <div className='card'>
@@ -13,19 +64,6 @@ function Card({props}) {
     <a href={props.linkedin} className='link' target='_blank'>LinkedIn</a>
     <a href={props.twitter} className='link' target='_blank'>Twitter</a>
     </div>
-  </div>
-}
-
-function App() {
-  const prop = {
-    name: 'Lokeshwar',
-    description: 'A TA in the 100xDevs Cohort 2.0',
-    interests: ['Ionic', 'Open Source', 'App Dev'],
-    linkedin: "https://www.linkedin.com",
-    twitter: "https://twitter.com"
-  };
-  return <div>
-    <Card props={prop}/>
   </div>
 }
 
