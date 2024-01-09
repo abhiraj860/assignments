166954635fb0dbf854fe73f359e9b4140314cfe0
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-09 18:38:42 +0530

    week-5-done

--- week-5/level-1/project-cards/frontend/src/App.jsx
+++ week-5/level-1/project-cards/frontend/src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function Card({props}) {
-  return <div>
-    <h1>{props.name}</h1>
-    <p>{props.description}</p>
-    {/* <p>{props.description}</p> */}
+  return <div className='card'>
+    <h1 className='name'>{props.name}</h1>
+    <p className='description'>{props.description}</p>
+    <h2 className='interestsHeader'>Interests</h2>
+    <ul className='interestsList'>
+      {props.interests.map((value, id)=>{return <li className='interestItem' key={id}>{value}</li>})}
+    </ul>
+    <div className='socialLinks'>
+    <a href={props.linkedin} className='link'>LinkedIn</a>
+    <a href={props.twitter} className='link'>Twitter</a>
+    </div>
   </div>
 }
 
