0ca872d68bcb096276d870580bb75ae40e0b0051
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-12 20:34:47 +0530

    db--schema-added

--- week-5/level-1/project-cards/backend/db.js
+++ week-5/level-1/project-cards/backend/db.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb+srv://abhiaditya860:ZHXUrjX1q1Rg18RV@cluster0.xnayn8w.mongodb.net/cardsCreator');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    interests: [String],
+    linkedIn: String,
+    twitter: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
