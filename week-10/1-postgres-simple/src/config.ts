5323d506e1afe6af9cd6025239203b069bcbea3b
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-09 18:37:02 +0530

    user-done

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
 
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+export const DB_URL = "postgres://gebsudbm:RJ13X0hPH8EVcacXUiN1JQlIEpf4YNL4@tai.db.elephantsql.com/gebsudbm";
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -10,6 +10,24 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
+    await client.connect();
+    try {
+        const createTableQuery = `CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL
+        );`;
+        await client.query(createTableQuery);
+        
+        const insertTableQuery = `
+            INSERT INTO users (username, password, name) VALUES ($1, $2, $3);
+        `; 
+        const insertData = await client.query(insertTableQuery, [username, password, name]);
+        return insertData;
+    } catch(err) {
+        console.log("Error here", err);
+    } 
     
 }
 
@@ -22,5 +40,8 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+    await client.connect();
+    const sqlQuery = `SELECT * FROM users WHERE ID = $1`;
+    const getQuery = await client.query(sqlQuery, [userId]);
+    return getQuery;
 }
